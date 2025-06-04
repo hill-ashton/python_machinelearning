433ccc157e3a5adb57787f6f7fd53c6af2bfeb39
Ashton Hill <157679596+hill-ashton@users.noreply.github.com>
2025-06-04 17:22:53 +0000

    compare UMAP and PCA dimensionality reduction to two dimensions

--- tSNE_UMAP-dimension-reduction.py
+++ tSNE_UMAP-dimension-reduction.py
@@ -57,3 +57,37 @@ ax.set_ylabel("t-SNE Component 2")
 ax.set_xticks([])
 ax.set_yticks([])
 plt.show()
+
+#apply UMAP to reduce the dimensionality to 2D
+umap_model = UMAP.UMAP(n_components=2, random_state=42, min_dist=0.5, spread=1,n_jobs=1)
+
+X_umap = umap_model.fit_transform(X_scaled)
+
+#plot the 2D UMAP projection result 
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111)
+ax.scatter(X_umap[:, 0], X_umap[:, 1], c=labels_, cmap='viridis', s=50, alpha=0.7, edgecolor='k')
+
+ax.set_title("2D UMAP Projection of 3D Data")
+ax.set_xlabel("UMAP Component 1", )
+ax.set_ylabel("UMAP Component 2", )
+ax.set_xticks([])
+ax.set_yticks([])
+plt.show()
+
+#apply PCA to reduce the dimensionality to 2D
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+
+
+fig = plt.figure(figsize=(8, 6))
+
+#plot the 2D PCA result (right)
+ax2 = fig.add_subplot(111)
+scatter2 = ax2.scatter(X_pca[:, 0], X_pca[:, 1], c=labels_, cmap='viridis', s=50, alpha=0.7, edgecolor='k')
+ax2.set_title("2D PCA Projection of 3-D Data")
+ax2.set_xlabel("PCA 1")
+ax2.set_ylabel("PCA 2")
+ax2.set_xticks([])
+ax2.set_yticks([])
+plt.show()
