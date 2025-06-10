cdc186f7eaf9ea0bbaa85c2897ee9c5c714ed970
Ashton Hill <157679596+hill-ashton@users.noreply.github.com>
2025-06-10 20:03:44 +0000

    add regression models and evaluate metrics with outliers

---
+++ regularization-in-linear-regression.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, r2_score
+
+#define function to display evaluation metrics
+def regression_results(y_true, y_pred, regr_type):
+
+    #regression metrics
+    ev = explained_variance_score(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred) 
+    mse = mean_squared_error(y_true, y_pred) 
+    r2 = r2_score(y_true, y_pred)
+    
+    print('Evaluation metrics for ' + regr_type + ' Linear Regression')
+    print('explained_variance: ',  round(ev,4)) 
+    print('r2: ', round(r2,4))
+    print('MAE: ', round(mae,4))
+    print('MSE: ', round(mse,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
+    print()
+
+#generate synthetic data
+noise=1
+np.random.seed(42)
+X = 2 * np.random.rand(1000, 1)
+y = 4 + 3 * X + noise*np.random.randn(1000, 1)  #linear relationship with some noise
+y_ideal =  4 + 3 * X
+#specify portion of the dataset to add outliers
+y_outlier = pd.Series(y.reshape(-1).copy())
+
+#identify indices where the feature variable X is greater than a certain threshold
+threshold = 1.5  #example threshold to add outliers for larger feature values
+outlier_indices = np.where(X.flatten() > threshold)[0]
+
+#add outliers at random locations within specified portion
+num_outliers = 5  #number of outliers to add
+selected_indices = np.random.choice(outlier_indices, num_outliers, replace=False)
+
+#modify the target values at these indices to create outliers
+y_outlier[selected_indices] += np.random.uniform(50, 100, num_outliers)
+
+#plot data with outliers and line of best fit
+plt.figure(figsize=(12, 6))
+
+#original data
+plt.scatter(X, y_outlier, alpha=0.4,ec='k', label='Original Data with Outliers')
+plt.plot(X, y_ideal,  linewidth=3, color='g',label='Ideal, noise free data')
+
+plt.xlabel('Feature (X)')
+plt.ylabel('Target (y)')
+plt.title('')
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(12, 6))
+
+#scatter plot of the original data with outliers
+plt.scatter(X, y, alpha=0.4,ec='k', label='Original Data without Outliers')
+plt.plot(X, y_ideal,  linewidth=4, color='g',label='Ideal, noise free data')
+
+plt.xlabel('Feature (X)')
+plt.ylabel('Target (y)')
+plt.title('')
+plt.legend()
+plt.show()
+
+#fit a simple linear regression model
+lin_reg = LinearRegression()
+lin_reg.fit(X, y_outlier)
+y_outlier_pred_lin = lin_reg.predict(X)
+
+#fit a ridge regression model
+ridge_reg = Ridge(alpha=1)
+ridge_reg.fit(X, y_outlier)
+y_outlier_pred_ridge = ridge_reg.predict(X)
+
+#fit a lasso regression model
+lasso_reg = Lasso(alpha=.2)
+lasso_reg.fit(X, y_outlier)
+y_outlier_pred_lasso = lasso_reg.predict(X)
