0844f624e6ad891bc034396b25878886849a8727
Ashton Hill <157679596+hill-ashton@users.noreply.github.com>
2025-06-18 22:14:22 +0000

    implemented seasonality function

--- rainfall-prediction-classifier.py
+++ rainfall-prediction-classifier.py
@@ -26,3 +26,16 @@ df = df.rename(columns={'RainToday': 'RainYesterday',
 #location selection
 df = df[df.Location.isin(['Melbourne','MelbourneAirport','Watsonia',])]
 df. info()
+
+#seasonality feature
+def date_to_season(date):
+    month = date.month
+    if (month == 12) or (month == 1) or (month == 2):
+        return 'Summer'
+    elif (month == 3) or (month == 4) or (month == 5):
+        return 'Autumn'
+    elif (month == 6) or (month == 7) or (month == 8):
+        return 'Winter'
+    elif (month == 9) or (month == 10) or (month == 11):
+        return 'Spring'
+    
